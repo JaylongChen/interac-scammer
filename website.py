606ca11319033d9037ed98903c1f3f65e6907e02
JaylongChen <jaylongchen@gmail.com>
2023-02-12 02:33:11 -0500

    made selenium script to repeated send information over website

---
+++ website.py
@@ -0,0 +1,51 @@
+# This website use prompt to get your e-transfer answers. 
+import requests
+import time
+import random
+import string
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# get word list
+word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+response = requests.get(word_site)
+word_list = response.content.splitlines()
+
+# some parameters
+url = "https://confirm-my-interac.com/en"
+question = 'password?'
+
+driver = webdriver.Firefox()
+driver.get(url)
+
+while(True):
+    index = random.randint(0, len(word_list) - 1)
+    num = random.randint(10, 1000)
+    letters = string.ascii_lowercase
+    answers = ''.join(random.choice(str(letters)) for i in range(20))
+
+    # fill out form
+    driver.find_element(By.NAME, "wpforms[fields][0]").send_keys(str(word_list[index]))
+    index = random.randint(0, len(word_list) - 1)
+    driver.find_element(By.NAME, "wpforms[fields][3]").send_keys(str(num))
+    index = random.randint(0, len(word_list) - 1)
+    driver.find_element(By.NAME, "wpforms[fields][4]").send_keys(str(word_list[index]))
+    driver.find_element(By.NAME, "wpforms[fields][5]").send_keys(question)
+    driver.find_element(By.NAME, "wpforms[fields][6]").send_keys(answers)
+
+    # click button
+    driver.find_element(By.ID, "wpforms-submit-108").click()
+
+    # go back to previous form page
+    
+    # driver.back()
+    # driver.execute_script("window.history.go(-1)")
+
+    # time.sleep(2)
+    # clear out form
+    # driver.find_element(By.NAME, "wpforms[fields][0]").clear()
+    # driver.find_element(By.NAME, "wpforms[fields][3]").clear()
+    # driver.find_element(By.NAME, "wpforms[fields][4]").clear()
+    # driver.find_element(By.NAME, "wpforms[fields][5]").clear()
+    # driver.find_element(By.NAME, "wpforms[fields][6]").clear()
+    driver.get(url)
